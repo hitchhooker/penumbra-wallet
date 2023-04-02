6699afa1cdb65cde234dddd21eabaa9700a9b3d8
Tommi Niemi <tommi@niemi.lol>
2023-04-02 05:17:49 +0300

    antumbra

--- icons/generate-icons.sh
+++ icons/generate-icons.sh
@@ -6,17 +6,9 @@ if ! [ -x "$(command -v cairosvg)" ]; then
 	exit 1
 fi
 
-curl https://raw.githubusercontent.com/penumbra-zone/penumbra/main/docs/images/penumbra-dark.svg --output penumbra-logo.svg
-SVG_FILE="penumbra-logo.svg"
-
-# remove comments to make black
-sed 's/<!--//g' $SVG_FILE >temp.svg
-sed -i 's/-->//g' temp.svg
+curl https://raw.githubusercontent.com/hitchhooker/antumbra/main/public/antumbra.svg --output penumbra-logo.svg
 
 # Generate icons with black background using cairosvg
-cairosvg temp.svg -o icon16.png -W 16 -H 16 --output-width 16
-cairosvg temp.svg -o icon48.png -W 48 -H 48 --output-width 48
-cairosvg temp.svg -o icon128.png -W 128 -H 128 --output-width 128
-
-# Remove the temporary SVG file
-rm temp.svg
+cairosvg penumbra-logo.svg -o icon16.png -W 16 -H 16 --output-width 16
+cairosvg penumbra-logo.svg -o icon48.png -W 48 -H 48 --output-width 48
+cairosvg penumbra-logo.svg -o icon128.png -W 128 -H 128 --output-width 128
